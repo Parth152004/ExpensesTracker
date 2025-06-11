9a253881046de6871e12e045b8a4d05d7fd16007
Parth152004 <patelparth1167@gmail.com>
2025-06-11 21:57:33 +0530

    solve issue that duplicate user not enter again

--- src/main/java/com/example/demo/Service/UserServiceImp.java
+++ src/main/java/com/example/demo/Service/UserServiceImp.java
@@ -3,6 +3,7 @@ package com.example.demo.Service;
 import com.example.demo.Entity.Users;
 import com.example.demo.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +18,14 @@ public class UserServiceImp implements UserService{
 
     @Override
     public ResponseEntity<Users> addUser(Users user) {
-        Users addedUser = userRepository.save(user);
-        return ResponseEntity.ok(addedUser);
+            Users existingUser = userRepository.findByEmail(user.getEmail());
+            if (existingUser != null) {
+                // Email already exists, return 409 Conflict
+                return ResponseEntity.ok(existingUser);
+            }else{
+            Users addedUser = userRepository.save(user);
+            return ResponseEntity.ok(addedUser);
+        }
     }
 
     @Override
